2af8b9ee06a7faacb8b3fcbcd12f381723678e86
Eole35 <polo@MacBook-Air-de-PoLo.local>
2026-09-26 20:08:14 +0200

    Fix integration configuration and test setup

--- custom_components/mon_panier/__init__.py
+++ custom_components/mon_panier/__init__.py
@@ -1,12 +1,15 @@
 """The Mon Panier integration."""
 
+from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
+import homeassistant.helpers.config_validation as cv
 
 from .const import DOMAIN
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the Mon Panier integration."""
     hass.data.setdefault(DOMAIN, {})
-
     return True
